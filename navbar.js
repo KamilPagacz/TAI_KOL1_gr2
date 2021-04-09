df4ee4203b44e936ab42ac951d31781047fefdf3
Kamil Pagacz <kamil22197@gmail.com>
2021-04-09 15:35:41 +0200

    bez zadania 4

---
+++ navbar.js
@@ -0,0 +1,6 @@
+let nav = '<h1>Nawigacja</h1><nav><a href="index.html">Home</a>' + 
+            '<a href="kontakt.html">Kontakt</a></nav>';
+
+let nav_elem = document.querySelector(".nav-bar");
+
+nav_elem.innerHTML = nav;
---
+++ plik.js
@@ -0,0 +1,6 @@
+let button2 = document.querySelector('.button2');
+let body = document.querySelector('body');
+
+button2.addEventListener('click', function () {
+    body.style.backgroundColor='green'
+});
